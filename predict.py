2300089b19f408c6179b54176d59269602c657fb
Ronak <106268100+RonSheoran123@users.noreply.github.com>
2023-09-10 23:38:50 +0530

    Create predict.py

---
+++ predict.py
@@ -0,0 +1,12 @@
+def predict_one(x_train,y_train,x_test,k):
+    distances=[]
+    for i in range(len(x_train)):
+        distance=((x_train[i,:]-x_test)**2).sum()
+        distances.append([distance,i])
+    distances=sorted(distances)
+    targets=[]
+    for i in range(k):
+        index_of_training_data=distance[i][1]
+        targets.append(y_train(index_of_training_data))
+        
+    return Counter(targets).most_common(1)[0][0]
